e264735878c6d59826a3650f69575d1d1d8cb2f7
YongRen <zhanxin13@gmail.com>
2017-06-28 17:16:15 +0800

    add blur background

--- src/MusicPlayer.js
+++ src/MusicPlayer.js
@@ -1,14 +1,36 @@
 var musicList = [{
   src: '//cloud.hunger-valley.com/music/ifyou.mp3',
   title: 'IF YOU',
-  auther: 'Big Bang',
+  author: 'Big Bang',
   imgUrl: "//pic.xiami.net/images/album/img81/23681/236811442023681.jpg@4e_1c_100Q_185w_185h"
 }, {
   src: '//cloud.hunger-valley.com/music/玫瑰.mp3',
   title: '玫瑰',
-  auther: 'The Beatles',
+  author: 'The Beatles',
   imgUrl: "//pic.xiami.net/images/album/img29/10029/1684161450154192.jpeg@4e_1c_100Q_185w_185h"
 },
+{
+  title: 'Preparation',
+  author: 'Hans Zimmer & Richard Harvey',
+  src: 'http://devtest.qiniudn.com/Preparation.mp3',
+  imgUrl: 'http://devtest.qiniudn.com/Preparation.jpg'
+},
+{
+  title: '回レ！雪月花',
+  author: '小倉唯',
+  src: 'http://devtest.qiniudn.com/回レ！雪月花.mp3',
+  imgUrl: 'http://devtest.qiniudn.com/回レ！雪月花.jpg',
+}, {
+  title: 'secret base~君がくれたもの~',
+  author: '茅野愛衣',
+  src: 'http://devtest.qiniudn.com/secret base~.mp3',
+  imgUrl: 'http://devtest.qiniudn.com/secret base~.jpg'
+}, {
+  title: 'あっちゅ～ま青春!',
+  author: '七森中☆ごらく部',
+  src: 'http://devtest.qiniudn.com/あっちゅ～ま青春!.mp3',
+  imgUrl: 'http://devtest.qiniudn.com/あっちゅ～ま青春!.jpg'
+}
 ]
 
 function data() {
@@ -29,7 +51,7 @@ function data() {
   function loadSongs(songObj) {
     music.src = songObj.src
     titleNode.innerText = songObj.title
-    authorNode.innerText = songObj.auther
+    authorNode.innerText = songObj.author
   }
 }
 
@@ -71,8 +93,11 @@ function createPlayer() {
     </div>
   </div>
   <div class='listSection'>
+    <div class='bgBlur'></div>
   </div>
   `
+      // <div class='listContent'></div>
+
   document.getElementById('root').appendChild(Player)
 }
 createPlayer()
@@ -94,6 +119,7 @@ function MusicPlayer() {
   var imgNode = document.querySelector('.musicBox .detailShow img')
 
   var listSecNode = document.querySelector('.musicBox .listSection')
+  var blurBgDiv = document.querySelector('.listSection .bgBlur')
   function createList(songArr) {
     var ulNode = document.createElement('ul')
     var html = ''
@@ -102,7 +128,7 @@ function MusicPlayer() {
       html += `<li data-index=${index}>
                 <span class='iconfont icon-yinle' ></span>
                 <div>${obj.title}</div>
-                <span>${obj.auther}</span>
+                <span>${obj.author}</span>
                </li>`
       index++
     })
@@ -111,6 +137,8 @@ function MusicPlayer() {
   }
   listSecNode.appendChild(createList(musicList))
 
+  // playList event handle
+  var lastIndex = -1
   document.querySelector('.musicBox .listSection ul').addEventListener("click", function (e) {
     if (event.target.tagName.toLowerCase() === 'ul') {
       return
@@ -123,32 +151,68 @@ function MusicPlayer() {
     liNode.classList.add('activeSongTtem')
 
     musicIndex = liNode.dataset.index
-    readySonge(musicList[musicIndex])
-    music.play()
-  },true)
+    console.log(` ${lastIndex} -- ${musicIndex}`)
 
+    if(lastIndex === musicIndex) { 
+      console.log('same song ') 
+      return
+    }
+    readySonge(musicList[musicIndex])
+    playBtn.classList.toggle('icon-bofang1')
+    playBtn.classList.toggle('icon-zanting')
+    // console.log(' start playing ...') 
+    music.play()
+    lastIndex = musicIndex
+  }, true)
+
+  function liSelectedAtIndex(index) {
+    let allLiNode = document.querySelectorAll('.musicBox .listSection ul li')
+    for (var i = 0; i <allLiNode.length; i++) {
+      allLiNode[i].classList.remove('activeSongTtem')
+    }
+    allLiNode[index].classList.add('activeSongTtem')
+  }
+
+  var isReady = false
   function readySonge(songObj) {
+    console.log("readySonge here ")
+    isReady = true
     music.src = songObj.src
     titleNode.innerText = songObj.title
-    authorNode.innerText = songObj.auther
+    authorNode.innerText = songObj.author
     imgNode.src = songObj.imgUrl
+    blurBgDiv.style.backgroundImage = `url(${songObj.imgUrl})`
   }
 
   playBtn.onclick = function () {
-    if (this.classList.contains('icon-bofang1')) {
+    if(!isReady) {
+      alert("请在歌曲列表中选择一首歌曲")
+      return
+    } 
+
+    if (!this.classList.contains('icon-bofang1')) {
       console.log('plause')
       music.pause()
     } else {
       console.log('play')
       music.play()
     }
+    
     this.classList.toggle('icon-bofang1')
     this.classList.toggle('icon-zanting')
   }
   forwardBtn.onclick = function () {
+    if(!isReady) {
+      alert("请在歌曲列表中选择一首歌曲")
+      return
+    } 
     loadNextMusic()
   }
   backBtn.onclick = function () {
+    if(!isReady) {
+      alert("请在歌曲列表中选择一首歌曲")
+      return
+    } 
     loadPreMusic()
   }
   barNode.onclick = function (e) {
@@ -179,17 +243,21 @@ function MusicPlayer() {
     musicIndex++
     musicIndex = musicIndex % musicList.length
     readySonge(musicList[musicIndex])
+    liSelectedAtIndex(musicIndex)
+    music.play()
   }
   function loadPreMusic() {
     musicIndex--
     musicIndex = (musicIndex + musicList.length) % musicList.length
     readySonge(musicList[musicIndex])
+    liSelectedAtIndex(musicIndex)
+    music.play()
   }
 
   // not using this time
   function wave(audioObj, canvasNode) {
     window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.msAudioContext;
-    var context=new window.AudioContext();
+    var context = new window.AudioContext();
     var analyser = context.createAnalyser()
     // var source = context.createMediaElementSource(audioObj)
     var source = context.createMediaStreamSource(audioObj)
@@ -199,7 +267,7 @@ function MusicPlayer() {
     console.dir(context)
     console.log(canvasNode)
     var ctx = canvasNode.getContext('2d')
-    
+
     // frameLooper()
     function frameLooper() {
       console.log(' ... ')
